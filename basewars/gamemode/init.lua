a6b61a565c5c65185d773b8c12145f02be4dc018
Chutnut <Christianbeddoes2@gmail.com>
2016-07-29 20:42:19 +0100

    Add files via upload

---
+++ basewars/gamemode/init.lua
@@ -0,0 +1,41 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+AddCSLuaFile("HUD.lua")
+
+include("shared.lua")
+include("HUD.lua")
+
+function GM:PlayerInitialSpawn(ply)
+	if (ply:GetNWInt("playerLvl") <= 0) then
+		ply:SetNWInt("playerLvl", 1)
+	end
+end
+
+function GM:OnNPCKilled(npc, attacker, inflictor)
+	attacker:SetNWInt("playerMoney", attacker:GetNWInt("playerMoney") + 100)
+	
+	attacker:SetNWInt("playerExp", attacker:GetNWInt("playerExp") + 100)
+	
+	checkForLevel(attacker)
+end
+
+function GM:PlayerDeath(victm, inflictor, attacker)
+	attacker:SetNWInt("playerMoney", attacker:GetNWInt("playerMoney") + 200)
+	
+	attacker:SetNWInt("playerExp", attacker:GetNWInt("playerExp") + 100)
+	
+	checkForLevel(attacker)
+end
+
+function checkForLevel(ply)
+	local expToLevel = (ply:GetNWInt("playerLvl") * 100) * 2
+	local curExp = ply:GetNWInt("playerExp")
+	local curLvl = ply:GetNWInt("playerLvl")
+	
+	if (curExp >= expToLevel) then
+		curExp = curExp - expToLevel
+		
+		ply:SetNWInt("playerExp", curExp)
+		ply:SetNWInt("playerLvl", curLvl + 1)
+	end
+end
